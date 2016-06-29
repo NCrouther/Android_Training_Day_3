6e30cb57b7eb7e18d713da537fc86bc15f415035
Nathan Crouther <nathan.crouther@abbott.com>
2016-06-29 10:04:42 -0700

    Chapter 10 implementation - Content Provider

---
+++ app/src/main/java/com/bignerdranch/android/initialtwittersyncadapter/contentprovider/TweetContentProvider.java
@@ -0,0 +1,164 @@
+package com.bignerdranch.android.initialtwittersyncadapter.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.TextUtils;
+
+public class TweetContentProvider extends ContentProvider {
+    private static final int TWEET_LIST = 1;
+    private static final int TWEET_ID = 2;
+    private static final int USER_LIST = 3;
+    private static final int USER_ID = 4;
+    private static final UriMatcher sUriMatcher;
+
+    private TweetDatabaseHelper mTweetDatabaseHelper;
+
+    static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        sUriMatcher.addURI(DatabaseContract.AUTHORITY,
+                DatabaseContract.Tweet.TABLE_NAME, TWEET_LIST);
+        sUriMatcher.addURI(DatabaseContract.AUTHORITY,
+                DatabaseContract.Tweet.TABLE_NAME + "/#", TWEET_ID);
+        sUriMatcher.addURI(DatabaseContract.AUTHORITY,
+                DatabaseContract.User.TABLE_NAME, USER_LIST);
+        sUriMatcher.addURI(DatabaseContract.AUTHORITY,
+                DatabaseContract.User.TABLE_NAME + "/#", USER_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mTweetDatabaseHelper = new TweetDatabaseHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        String tableName;
+        Cursor cursor;
+        switch (sUriMatcher.match(uri)) {
+            case TWEET_LIST:
+                tableName = DatabaseContract.Tweet.TABLE_NAME;
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = DatabaseContract.Tweet.TWEET_ID + " DESC";
+                }
+                cursor = mTweetDatabaseHelper.getReadableDatabase()
+                        .query(tableName, projection, selection, selectionArgs,
+                                null, null, sortOrder);
+                break;
+            case TWEET_ID:
+                tableName = DatabaseContract.Tweet.TABLE_NAME;
+                if (TextUtils.isEmpty(selection)) {
+                    selection = DatabaseContract.Tweet.TWEET_ID +
+                            " = " + uri.getLastPathSegment();
+                } else {
+                    selection = selection + " AND " + DatabaseContract.Tweet.TWEET_ID +
+                            " = " + uri.getLastPathSegment();
+                }
+                cursor = mTweetDatabaseHelper.getReadableDatabase()
+                        .query(tableName, projection, selection, selectionArgs,
+                                null, null, sortOrder);
+                break;
+            case USER_LIST:
+                tableName = DatabaseContract.User.TABLE_NAME;
+                if (TextUtils.isEmpty(sortOrder)) {
+                    sortOrder = DatabaseContract.User.USER_ID + " DESC";
+                }
+                cursor = mTweetDatabaseHelper.getReadableDatabase()
+                        .query(tableName, projection, selection, selectionArgs,
+                                null, null, sortOrder);
+                break;
+            case USER_ID:
+                tableName = DatabaseContract.User.TABLE_NAME;
+                if (TextUtils.isEmpty(selection)) {
+                    selection = DatabaseContract.User.USER_ID +
+                            " = " + uri.getLastPathSegment();
+                } else {
+                    selection = selection + " AND " + DatabaseContract.User.USER_ID +
+                            " = " + uri.getLastPathSegment();
+                }
+                cursor = mTweetDatabaseHelper.getReadableDatabase()
+                        .query(tableName, projection, selection, selectionArgs,
+                                null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported Uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (sUriMatcher.match(uri)) {
+            case TWEET_LIST:
+                return DatabaseContract.LIST_CONTENT_TYPE;
+            case TWEET_ID:
+                return DatabaseContract.SINGLE_CONTENT_TYPE;
+            case USER_LIST:
+                return DatabaseContract.LIST_CONTENT_TYPE;
+            case USER_ID:
+                return DatabaseContract.SINGLE_CONTENT_TYPE;
+            default:
+                throw new IllegalArgumentException("Unsupported Uri: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        long insertedRowId = 0;
+        switch (sUriMatcher.match(uri)) {
+            case TWEET_LIST:
+                insertedRowId = mTweetDatabaseHelper.getWritableDatabase().insert(
+                        DatabaseContract.Tweet.TABLE_NAME, null, contentValues);
+                break;
+            case USER_LIST:
+                insertedRowId = mTweetDatabaseHelper.getWritableDatabase().insert(
+                        DatabaseContract.User.TABLE_NAME, null, contentValues);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported Uri: " + uri);
+        }
+        if (insertedRowId > 0) {
+            notifyUriChanges(uri);
+            return ContentUris.withAppendedId(uri, insertedRowId);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int deletedCount;
+        switch (sUriMatcher.match(uri)) {
+            case TWEET_LIST:
+                deletedCount = mTweetDatabaseHelper.getWritableDatabase().delete(
+                        DatabaseContract.Tweet.TABLE_NAME, selection, selectionArgs);
+                break;
+            case USER_LIST:
+                deletedCount = mTweetDatabaseHelper.getWritableDatabase().delete(
+                        DatabaseContract.User.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported Uri: " + uri);
+        }
+        if (deletedCount > 0) {
+            notifyUriChanges(uri);
+        }
+        return deletedCount;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+
+    private void notifyUriChanges(Uri uri) {
+        getContext().getContentResolver().notifyChange(uri, null);
+    }
+}
