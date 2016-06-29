239842e519011520ed201a992081318fa399e5a8
Nathan Crouther <nathan.crouther@abbott.com>
2016-06-29 08:54:03 -0700

    Initial commit of template

---
+++ app/src/main/java/com/bignerdranch/android/initialtwittersyncadapter/controller/AuthenticationActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.initialtwittersyncadapter.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private static final String TAG = "AuthenticationActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
